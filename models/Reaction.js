5ae855b10aaa285796220add2c08d10cdae32a34
Daniel Xu <xudh47@gmail.com>
2022-11-05 23:27:47 -0700

    Updated Reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,4 +1,26 @@
+const { ObjectID, ObjectId } = require("bson");
 const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: ObjectId,
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  username: {
+    type: Sting,
+    required: true,
+  },
+  createdAt: {
+    date: Date,
+    default: Date.now(),
+  },
+});
 
 const Reaction = model("reaction", reactionSchema);
 
