b66dbcf532e33d2bbc79ca80b648e6b4bce2bd82
Daniel Xu <xudh47@gmail.com>
2022-11-05 22:35:45 -0700

    Updated User model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,35 @@
 const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      id: false,
+    },
+  }
+);
+
+// Virtual for friend count
+userSchema.virtual("userCount").get(function () {
+  return this.friends.length;
+});
 
 const User = model("user", userSchema);
 
