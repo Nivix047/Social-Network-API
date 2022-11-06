8b599e0c0f9105c583f153acbc6efca853f5bb41
Daniel Xu <xudh47@gmail.com>
2022-11-05 22:59:37 -0700

    Updated Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,36 @@
 const { Schema, model } = require("mongoose");
 
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      id: false,
+    },
+  }
+);
+
+// Virtual for thought count
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
 const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
